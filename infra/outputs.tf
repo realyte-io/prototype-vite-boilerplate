775ab2343848deeb6129929a4df789ddf0067f24
benpaffrath <ben.paffrath@panocreation.de>
2024-05-18 18:13:33 +0200

    move to script

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -3,24 +3,9 @@ output "aws_region" {
     value       = var.region
 }
 
-output "cognito_user_pool_arn" {
-    value       = module.auth.cognito_user_pool.arn
-    description = "The ARN of the Cognito user pool to use for authentication"
-}
-
 output "cognito_user_pool_id" {
     description = "The ID of the Cognito user pool to use for authentication"
     value       = module.auth.cognito_user_pool.id
-}
-
-output "cognito_user_pool_username_attributes" {
-    description = "The user attributes that are used as username"
-    value       = module.auth.cognito_user_pool.username_attributes
-}
-
-output "cognito_user_pool_password_policy" {
-    description = "The password policy of the Cognito user pool"
-    value       = module.auth.cognito_user_pool.password_policy
 }
 
 output "cognito_user_pool_client_id" {
